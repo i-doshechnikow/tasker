e70ae042e54d40384dbe2336a9f626949592bf2b
i-doshechnikow <77616522+i-doshechnikow@users.noreply.github.com>
2022-08-10 22:00:43 +0300

    max sub

---
+++ maxSubDp.js
@@ -0,0 +1,25 @@
+export default function dpMaximumSubarray(inputArray) {
+  let maxSum = -Infinity;
+  let currentSum = 0;
+
+  let maxStartIndex = 0;
+  let maxEndIndex = inputArray.length - 1;
+  let currentStartIndex = 0;
+
+  inputArray.forEach((currentNumber, currentIndex) => {
+    currentSum += currentNumber;
+
+    if (maxSum < currentSum) {
+      maxSum = currentSum;
+      maxStartIndex = currentStartIndex;
+      maxEndIndex = currentIndex;
+    }
+
+    if (currentSum < 0) {
+      currentSum = 0;
+      currentStartIndex = currentIndex + 1;
+    }
+  });
+
+  return inputArray.slice(maxStartIndex, maxEndIndex + 1);
+}
