10711124cbf6032a52e23873706c95474921da47
i-doshechnikow <77616522+i-doshechnikow@users.noreply.github.com>
2022-08-15 23:21:12 +0300

    add findKey func

---
+++ findKey.js
@@ -0,0 +1,11 @@
+const findKey = (obj, fn) =>
+  Object.keys(obj).find((key) => fn(obj[key], key, obj));
+
+let test = findKey(
+  {
+    barney: { age: 36, active: true },
+    fred: { age: 40, active: false },
+    pebbles: { age: 1, active: true },
+  },
+  (x) => x["active"]
+); // 'barney'
